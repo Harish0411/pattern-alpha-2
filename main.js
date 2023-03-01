982b2eab8e7c381a50e6cf30a4be0965c22c12bf
Harish0411 <hariharanramasamy98@gmail.com>
2023-03-01 10:43:04 +0530

    print alphabet pattern-2

---
+++ main.js
@@ -0,0 +1,10 @@
+var Alpha=65;		
+for(var i=1; i<=5; i++){
+	 for(var j=1; j<=i; j++){
+	     document.write(String.fromCharCode(Alpha) + " ");
+		
+		 }
+	 document.write("<br>");
+	  Alpha++;
+	 }
+	 
